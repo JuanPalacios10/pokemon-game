6efbf7f6f89cd3ec74737ead80bd79635d08de41
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-06-08 20:45:58 -0500

    style: add value error when dataset not found a pokemon

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -7,10 +7,9 @@ class Dataset:
         ruta = os.path.join(os.path.dirname(__file__), "pokedex.csv")
         self.data = pd.read_csv(ruta)
 
-    def get_pokemon_by_name(self, name: str) -> dict[str, int | str] | str:
+    def get_pokemon_by_name(self, name: str) -> dict[str, int | str]:
         result = self.data[self.data["Nombre"] == name]
         if not result.empty:
             return result.iloc[0].to_dict()
-        else:
-            return f"Pokemon {name} not found."
 
+        raise ValueError(f"Pokemon with name '{name}' not found in the dataset.")
