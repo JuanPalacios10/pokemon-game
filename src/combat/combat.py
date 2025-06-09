4bf6dd7a25e55bf643e53323222bdca083fb19e6
Juan Miguel <juan.miguel.palacios@correounivalle.edu.co>
2025-06-08 22:47:39 -0500

    feature: add next turn in combat

--- src/combat/combat.py
+++ src/combat/combat.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import random
 from src.trainers.trainers import Enemy, Player, Trainer
 from src.utils.effectiveness import effectiveness
 
@@ -15,26 +16,61 @@ class Combat:
         self.__state = CombatState.START
         self.__players = (player, enemy)
         self.__current_attack = ""
-        self.__turn = 0
         self.__winner = None
+        self.__next_turn()
+
+    def __next_turn(self) -> None:
+        speed_pokemon_player = self.__players[0].get_current_pokemon().get_speed()
+        speed_pokemon_enemy = self.__players[1].get_current_pokemon().get_speed()
+
+        if speed_pokemon_player > speed_pokemon_enemy:
+            self.__turn = 0
+            self.__state = CombatState.PLAYER_TURN
+            return
+
+        if speed_pokemon_player < speed_pokemon_enemy:
+            self.__turn = 1
+            self.__state = CombatState.ENEMY_TURN
+            return
+
+        self.__turn = random.choice([0, 1])
 
     def get_info_player(self):
         player_name = self.__players[0].get_name()
         current_pokemon = self.__players[0].get_current_pokemon()
         health = self.__players[0].get_current_pokemon_health()
+        live_pokemon = self.__players[0].get_live_pokemon()
+
+        return {
+            "player_name": player_name,
+            "pokemon_name": current_pokemon.get_name(),
+            "pokemon_health": health,
+            "pokemon_attack_1": current_pokemon.get_move_1_name(),
+            "pokemon_attack_2": current_pokemon.get_move_2_name(),
+            "pokemon_super_attack": current_pokemon.get_super_move_name(),
+            "live_pokemon": live_pokemon,
+        }
 
     def get_info_enemy(self):
         enemy_name = self.__players[1].get_name()
         current_pokemon = self.__players[1].get_current_pokemon()
         health = self.__players[1].get_current_pokemon_health()
+        live_pokemon = self.__players[1].get_live_pokemon()
+
+        return {
+            "enemy_name": enemy_name,
+            "pokemon_name": current_pokemon.get_name(),
+            "pokemon_health": health,
+            "pokemon_attack_1": current_pokemon.get_move_1_name(),
+            "pokemon_attack_2": current_pokemon.get_move_2_name(),
+            "pokemon_super_attack": current_pokemon.get_super_move_name(),
+            "live_pokemon": live_pokemon,
+        }
 
     def get_state(self) -> CombatState:
         return self.__state
 
-    def get_current_attack(self) -> str | None:
-        return self.__current_attack
-
-    def __next_turn(self) -> None:
+    def __next_trainer(self) -> None:
         if self.__turn == 0:
             self.__turn = 1
             self.__state = CombatState.ENEMY_TURN
@@ -85,7 +121,7 @@ class Combat:
     def __calculate_damage(
         self, current_trainer: Trainer, next_trainer: Trainer
     ) -> int:
-        level = 15
+        level = 20
         damage = (
             (
                 (((2 * level) // 5) + 2)
@@ -107,7 +143,7 @@ class Combat:
     def set_attack(self, attack: str) -> int:
         self.__current_attack = attack
         current_trainer = self.__players[self.__turn]
-        self.__next_turn()
+        self.__next_trainer()
         next_trainer = self.__players[self.__turn]
 
         damage = self.__calculate_damage(
@@ -126,6 +162,7 @@ class Combat:
 
         if not trainer.is_current_pokemon_alive():
             trainer.set_pokemon()
+            self.__next_turn()
 
     def enemy_choose_attack(self):
         pass
